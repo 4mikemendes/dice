bc6d5824603d73d2022547f38771b8d8ce36ab81
이혜진 <4mikemendes@gmail.com>
2021-07-10 06:42:26 +0200

    event

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // dice
 let numberEl = document.getElementById("number-el")
-console.log(numberEl)
 
 function dice() {
 let randomNumber = Math.floor(Math.random() * 6) + 1
 return randomNumber
 }
-console.log(dice())
 
-numberEl.textContent = dice()
+function Roll() {
+  numberEl.textContent = dice()
+
+}
