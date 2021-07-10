8236d0f6093b39bcb42ba76e7026eec386b29228
이혜진 <4mikemendes@gmail.com>
2021-07-10 06:07:33 +0200

    displayNumber

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // dice
+let numberEl = document.getElementById("number-el")
+console.log(numberEl)
+
 function dice() {
 let randomNumber = Math.floor(Math.random() * 6) + 1
 return randomNumber
 }
 console.log(dice())
+
+numberEl.textContent = dice()
